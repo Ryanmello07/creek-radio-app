3e588c02f1d96fb8af6eb0f8876c67aaa9a1b8a0
Ryanmello07 <67509637+Ryanmello07@users.noreply.github.com>
2026-03-17 06:35:53 -0700

    Fix web white screen and audio service

--- index.js
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-
-if (Platform.OS !== 'web') {
-  const TrackPlayer = require('react-native-track-player').default;
-  const { PlaybackService } = require('./services/PlaybackService');
-  TrackPlayer.registerPlaybackService(() => PlaybackService);
-}
-
-require('expo-router/entry');
---
+++ services/PlaybackService.native.ts
@@ -0,0 +1,9 @@
+import TrackPlayer, { Event } from 'react-native-track-player';
+
+async function PlaybackService() {
+  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.stop());
+}
+
+TrackPlayer.registerPlaybackService(() => PlaybackService);
